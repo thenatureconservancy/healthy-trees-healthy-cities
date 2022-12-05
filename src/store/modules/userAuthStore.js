ed9d1a54cf0a5f95f62e70d60efe1dbf0c26610d
Matt Silveira <msilveira@tnc.org>
2022-12-05 15:34:21 -0700

    turn off es lint

--- src/store/modules/userAuthStore.js
+++ src/store/modules/userAuthStore.js
@@ -1,3 +1,36 @@
+import AWS from 'aws-sdk';
+// var AWS = require('aws-sdk');
+var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({
+  apiVersion: '2016-04-18',
+  region: 'us-east-1',
+});
+
+// ES AWS Modules
+import {
+  CognitoUserPool,
+  AuthenticationDetails,
+  CognitoUserAttribute,
+  CognitoUser,
+} from 'amazon-cognito-identity-js';
+
+// var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+// var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
+
+// test vars
+const testUserName = 'matthew.d.silveira@gmail.com';
+const testPassword = 'Pass1234!';
+const testConfirmCode = '513754';
+
+// cognito setup SDK setup
+const clientId = '2k29aaohc7petefk0elgobd5m5';
+const userPoolId = 'us-east-1_xin9uKNC7';
+const poolData = {
+  UserPoolId: userPoolId, // Your user pool id here
+  ClientId: clientId, // Your client id here
+};
+
+const userPool = new CognitoUserPool(poolData);
+
 const state = () => ({});
 
 const getters = {};
