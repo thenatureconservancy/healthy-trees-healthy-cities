c54343b42e391819f356a819904100b05daa14d7
Matt Silveira <msilveira@tnc.org>
2022-12-05 17:10:43 -0700

    got nascience cognito pool to work

--- src/components/authentication/TheLoginForm.vue
+++ src/components/authentication/TheLoginForm.vue
@@ -5,7 +5,7 @@
     style="width: 80vw"
     class="q-mb-sm"
     outlined
-    v-model="pName"
+    v-model="username"
     label="Email"
   />
   <q-input
@@ -13,14 +13,61 @@
     style="width: 80vw"
     class="q-mb-sm"
     outlined
-    v-model="pName"
+    v-model="password"
     label="Password"
   />
-  <q-btn size="18px" color="primary" class="q-mb-xs q-mt-md">Login</q-btn>
+
+  <q-btn @click="login" size="18px" color="primary" class="q-mb-xs q-mt-md"
+    >Login</q-btn
+  >
+  <q-btn
+    @click="accountCreate"
+    size="18px"
+    color="primary"
+    class="q-mb-xs q-mt-md"
+    >Test Account Create</q-btn
+  >
+  <q-btn
+    @click="confirmSignup"
+    size="18px"
+    color="primary"
+    class="q-mb-xs q-mt-md"
+    >Confirm Sign Up</q-btn
+  >
+  <q-btn
+    @click="getCurrentUserIdToken"
+    size="18px"
+    color="primary"
+    class="q-mb-xs q-mt-md"
+    >getCurrentUserIdToken</q-btn
+  >
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    accountCreate() {
+      this.$store.dispatch('createAccount');
+    },
+    login() {
+      console.log(this.username, this.password);
+      this.$store.dispatch('login');
+    },
+    confirmSignup() {
+      this.$store.dispatch('confirmSignup');
+    },
+
+    getCurrentUserIdToken() {
+      this.$store.dispatch('getCurrentUserIdToken');
+    },
+  },
+};
 </script>
 
 <style></style>
--- src/store/modules/userAuthStore.js
+++ src/store/modules/userAuthStore.js
@@ -1,8 +1,9 @@
 import AWS from 'aws-sdk';
-// var AWS = require('aws-sdk');
-var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({
+// import AWS from 'aws-sdk/client-s3'; // this is the newest version but wont install
+
+const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({
   apiVersion: '2016-04-18',
-  region: 'us-east-1',
+  region: 'us-west-2',
 });
 
 // ES AWS Modules
@@ -19,17 +20,18 @@ import {
 // test vars
 const testUserName = 'matthew.d.silveira@gmail.com';
 const testPassword = 'Pass1234!';
-const testConfirmCode = '513754';
+const testConfirmCode = '269784';
 
 // cognito setup SDK setup
-const clientId = '2k29aaohc7petefk0elgobd5m5';
-const userPoolId = 'us-east-1_xin9uKNC7';
+const clientId = '101i663u1v1bhfgo3le89daot9';
+const userPoolId = 'us-west-2_y6e0iz7MX';
 const poolData = {
   UserPoolId: userPoolId, // Your user pool id here
   ClientId: clientId, // Your client id here
 };
 
 const userPool = new CognitoUserPool(poolData);
+console.log(userPool);
 
 const state = () => ({});
 
@@ -38,18 +40,67 @@ const getters = {};
 const mutations = {};
 
 const actions = {
-  //   async createActivity(context, params) {
-  //     const response = await context.dispatch('protectedApiRequest', {
-  //       route: `task`,
-  //       type: 'POST',
-  //       body: params,
-  //     });
-  //     if (response.status === 200) {
-  //       // request the updated activities object from the database
-  //       await context.dispatch('getOuActivities');
-  //       return true;
-  //     }
-  //   },
+  async createAccount() {
+    var params = {
+      ClientId: clientId /* required */,
+      Password: testPassword /* required */,
+      Username: testUserName /* required */,
+    };
+    cognitoidentityserviceprovider.signUp(params, function (err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else console.log(data); // successful response
+    });
+  },
+  confirmSignup() {
+    var params = {
+      ClientId: clientId /* required */,
+      ConfirmationCode: testConfirmCode /* required */,
+      Username: testUserName /* required */,
+    };
+    cognitoidentityserviceprovider.confirmSignUp(params, function (err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else console.log(data); // successful response
+    });
+  },
+  async login() {
+    var authenticationData = {
+      Username: testUserName,
+      Password: testPassword,
+    };
+    const authenticationDetails = new AuthenticationDetails(authenticationData);
+    const cognitoUser = new CognitoUser({
+      Username: testUserName,
+      Pool: userPool,
+    });
+    // let context = this;
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: function (result) {
+        console.log(result);
+        // context.userId = result.idToken.payload.sub;
+        // context.username = result.idToken.payload.email;
+        // console.log(context);
+      },
+      onFailure: function (err) {
+        console.log(err);
+        console.log(err.message);
+        // alert(err.message || JSON.stringify(err));
+      },
+      newPasswordRequired: function (userAttributes, requiredAttributes) {
+        // redirect to change-password page
+        console.log('new password required');
+      },
+    });
+  },
+  async getCurrentUserIdToken() {
+    const cognitoUser = userPool.getCurrentUser();
+    let idToken;
+    await cognitoUser.getSession(async function (err, session) {
+      console.log(session);
+      idToken = session.idToken.jwtToken;
+    });
+    console.log(idToken);
+    return idToken;
+  },
 };
 
 export default {
